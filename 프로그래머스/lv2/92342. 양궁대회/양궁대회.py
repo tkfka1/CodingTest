b3b256e792d29de0a1ff2674ffb3face7639b786
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-01-06 17:06:32 +0900

    [level 2] Title: 양궁대회, Time: 386.70 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,67 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+def solution(n, info):
+    global answer, result
+
+    def score(ryan):
+        s = 0
+        for i in range(11):
+            if ryan[i] == info[i] == 0:
+                continue
+            if ryan[i] > info[i]:
+                s += 10 - i
+            else:
+                s -= 10 - i
+        return s
+
+    def dfs(idx, left, ryan):
+        global answer, result
+        if idx == -1 and left:
+            return
+        if left == 0:
+            s = score(ryan)
+            if result < s:
+                answer = ryan[:]
+                result = s
+            return
+        for i in range(left, -1, -1):
+            ryan[idx] = i
+            dfs(idx-1, left-i, ryan)
+            ryan[idx] = 0
+
+    answer = [0 for _ in range(11)]
+    result = 0
+    dfs(10, n, [0 for _ in range(11)])
+    return answer if result != 0 else [-1]
