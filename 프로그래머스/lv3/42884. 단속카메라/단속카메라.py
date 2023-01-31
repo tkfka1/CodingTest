193aa2920c6950c0076739e472e5549e274e07a1
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-01-31 18:29:30 +0900

    [level 3] Title: 단속카메라, Time: 15.94 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,17 @@
+def solution(r):
+    answer = 0
+    
+    r.sort(key=lambda x:(x[1], x[0]))
+    # print(r)
+    
+    while(len(r)!=0):
+        idx=0
+        camera=r[0][1]
+        for i in range(len(r)):
+            if r[idx][0]<=camera:
+                r.pop(idx)
+            else:
+                idx+=1
+
+        answer+=1
+    return answer
