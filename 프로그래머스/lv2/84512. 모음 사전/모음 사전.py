072b52bad825245126654f69c563b7e1eb02a579
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-02-05 08:11:16 +0900

    [level 2] Title: 모음 사전, Time: 0.80 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,9 @@
 import itertools
 def solution(word):
     answer = 0
-    mo = ["A","E","I","O","U"]
     li = ["A","E","I","O","U"]
     for i in range(2,6):
-        for ii in itertools.product(mo, repeat=i):
+        for ii in itertools.product(["A","E","I","O","U"], repeat=i):
             li.append(''.join(ii))
-    
-    li = sorted(li)
-    
-    return li.index(word)+1
+
+    return sorted(li).index(word)+1
