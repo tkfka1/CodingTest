4e32d5557f2e2de8992e5ce1a71f53c4a000c94a
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-02-09 03:38:19 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,18 +1,21 @@
-from itertools import permutations
+# from itertools import permutations
+
+# def solution(t, p):
+#     answer = 0
+    
+#     tlist = []
+#     for i in range(len(t)):
+#         temp = []
+#         for ii in range(len(p)):
+#             temp.append(t[i+ii])
+#         tlist.append(temp)
+#         if i == len(t)-len(p):
+#             break
+
+#     for i in tlist:
+#         if int(p) >= int(''.join(i)):
+#             answer += 1
+#     return answer
 
 def solution(t, p):
-    answer = 0
-    
-    tlist = []
-    for i in range(len(t)):
-        temp = []
-        for ii in range(len(p)):
-            temp.append(t[i+ii])
-        tlist.append(temp)
-        if i == len(t)-len(p):
-            break
-
-    for i in tlist:
-        if int(p) >= int(''.join(i)):
-            answer += 1
-    return answer
+    return sum([1 for i in range(len(t) - len(p) + 1) if int(p) >= int(t[i:i+len(p)])])
