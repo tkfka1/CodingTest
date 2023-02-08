977c0e06d060632384eeb37d8eb78cfaa33fa99e
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-02-09 03:28:24 +0900

    [unrated] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,14 +1,19 @@
+# def solution(s, skip, index):
+#     answer = ''
+    
+#     alpa = [chr(i) for i in range(97,123)]
+    
+#     for i in skip:
+#         alpa.remove(i)
+    
+#     for i in s:
+#         x = alpa.index(i)+index
+#         x = x%len(alpa)
+#         answer += alpa[x]
+    
+#     return answer
+
 def solution(s, skip, index):
-    answer = ''
-    
     alpa = [chr(i) for i in range(97,123)]
-    
-    for i in skip:
-        alpa.remove(i)
-    
-    for i in s:
-        x = alpa.index(i)+index
-        x = x%len(alpa)
-        answer += alpa[x]
-    
-    return answer
+    alpa = [char for char in alpa if char not in skip]
+    return ''.join([alpa[(alpa.index(char) + index) % len(alpa)] for char in s])
