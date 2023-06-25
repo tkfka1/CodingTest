7e72e7b7042b18e8d385b497722ec40129eecc3d
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-06-25 15:37:46 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,8 @@
+def solution(a, b, c):
+    if a == b or a == c or b == c:
+        if a == b and b == c:
+            return (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+        else:
+            return (a+b+c) * (a**2 + b**2 + c**2)
+    else:
+        return a+b+c
