3981b0534380765ded7722554575e8e8ef43166c
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-08-10 22:47:54 +0900

    [unrated] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,9 @@
+def solution(n, slicer, num_list):
+    a,b,c = slicer
+    if n == 1:
+        return num_list[:b+1]
+    if n == 2:
+        return num_list[a:]
+    if n == 3:
+        return num_list[a:b+1]
+    return [num_list[i] for i in range(a,b+1,c)]
