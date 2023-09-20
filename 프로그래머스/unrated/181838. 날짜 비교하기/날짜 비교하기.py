acbc2f467d502a5d5687212fee21d8c5053dbdae
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-09-21 07:40:56 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,15 @@
+def solution(date1, date2):
+    if date1 == date2:
+        return 0
+    if date1[0] > date2[0]:
+        return 0
+    if date1[0] < date2[0]:
+        return 1
+    if date1[1] > date2[1]:
+        return 0
+    if date1[1] < date2[1]:
+        return 1
+    if date1[2] > date2[2]:
+        return 0
+    if date1[2] < date2[2]:
+        return 1
