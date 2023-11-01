109184fd89cf9eda51aa1e1b159dae2a7d2c3f4b
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-11-01 09:24:11 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,7 @@
+n,m = map(int, input().split())
+temp = [i+1 for i in range(n)]
+for i in range(m):
+        a,b = map(int,input().split())
+        a -= 1
+        temp[a:b] = reversed(temp[a:b])
+print(" ".join(map(str,temp)))
