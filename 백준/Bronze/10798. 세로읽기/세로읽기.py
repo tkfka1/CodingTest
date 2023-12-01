dd5ab3d623bc095b5422f71d69d98940bf68f638
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-12-01 22:23:55 +0900

    [Bronze I] Title: 세로읽기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,15 @@
+li = [["!" for ii in range(15)] for i in range(5)]
+
+for y in range(5):
+    temp = input()
+    for x in range(len(temp)):
+        li[y][x] = temp[x]
+
+answer = ""
+
+for x in range(15):
+    for y in range(5):
+        if li[y][x] != "!":
+            answer += li[y][x]   
+            
+print(answer)
