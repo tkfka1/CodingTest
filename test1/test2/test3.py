fc36ce200031630177eda938bd35ddb767e5010a
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-12-17 23:52:27 +0900

    Create test3.py

---
+++ test1/test2/test3.py
@@ -0,0 +1,41 @@
+def find(x):
+    stack = 1
+    plus = 1
+    time = 0
+
+    if x == 1:
+        print("1/1")
+        return
+    else:
+        while stack < x:
+            plus += 4
+            stack += plus
+            time += 1
+
+        if stack == x:
+            print("1/" + str(time*2+1))
+            return
+        
+        if stack-plus+1 == x:
+            print("1/" + str(time*2))
+            return
+        
+        a,b = 1,time*2
+        temp = stack-plus+(time*2)
+        if temp >= x:
+            temp2 = temp - x + 1
+            temp3 = b+1 - temp2
+            print(str(temp3)+"/"+str(temp2))
+            return
+        else:
+            b+=1
+            temp2 = stack - x
+            b -= temp2
+            a += temp2
+            print(str(a)+"/"+str(b))
+            return
+
+
+
+
+find(2)
