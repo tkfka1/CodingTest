6c413532ffad593de211ddaadeb9384b16d97a98
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-12-18 00:30:11 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -0,0 +1,15 @@
+def sol(x):
+    if x == 1:
+        print(1)
+    
+    else:
+        a = 1
+        b = 6
+        stack = 1
+        while a < x:
+            a += b
+            b += 6
+            stack += 1
+        print(stack)
+
+sol(int(input()))
