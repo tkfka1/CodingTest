2eae6bfe979eca0f9b6a6b355dc779dbb311e2f6
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2024-01-08 19:08:54 +0900

    Create test6.py

---
+++ test1/test2/test6.py
@@ -0,0 +1,24 @@
+while True:
+    x = int(input())
+    temp = f"{x} = "
+    temp_li = []
+    if x == -1:
+        break
+    else:
+        if x != 0:
+            for i in range(1,x//2 + 1):
+                if x%i == 0:
+                    temp_li.append(i)
+            if x == sum(temp_li):
+                if len(temp_li) == 1:
+                    temp += f"{str(temp_li[0])}"
+                else:
+                    for i in range(len(temp_li)-1):
+                        temp += f"{str(temp_li[i])} + "
+                    temp += f"{str(temp_li[-1])}"
+            else:
+                temp = f"{x} is NOT perfect."
+        
+            print(temp)
+                
+            
