74552e2dcb437dde04d44a6bd437d5ce53ccaf93
mztkfka <tkfka1@korea.com>
2024-01-11 13:09:06 +0900

    t

---
+++ test1/test3/test1.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+a = [False,False] + [True]*(n-1)
+primes=[]
+
+for i in range(2,n+1):
+  if a[i]:
+    primes.append(i)
+    for j in range(2*i, n+1, i):
+        a[j] = False
+
+answer = n
+        
+for i in primes:
+    if answer == 1:
+        break
+    if i == answer:
+        print(answer)
+        break
+    while i <= answer:
+        if answer == 1:
+            break
+        if i == answer:
+            print(answer)
+            break
+        if answer%i == 0:
+            print(i)
+            answer = answer//i
+        else:
+            break
