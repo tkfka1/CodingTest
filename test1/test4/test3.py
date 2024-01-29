2763e55ce068b6bcbb9ce88f8c3a3f20f0cc9ebe
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2024-01-29 14:17:00 +0900

    Update test3.py

--- test1/test4/test3.py
+++ test1/test4/test3.py
@@ -27,3 +27,5 @@ PersonWithoutSelf.set_age(25)
 
 # 동일한 클래스 메서드 호출
 print(PersonWithoutSelf.greet())  # "Hello, my name is Bob and I am 25 years old."이 경우 PersonWithoutSelf 클래스는 모든 인스턴스에 대해 동일한 상태(즉, 최근에 설정된 이름과 나이)를 공유합니다. self가 없기 때문에, 각 인스턴스는 고유한 상태를 가질 수 없습니다.
+
+
